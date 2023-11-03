017cadec95b93e262b92387a94988b8189feb802
GeorgTheVic <georgthevic@gmail.com>
2023-11-03 19:08:07 +0600

    added topic about events

--- index.js
+++ index.js
@@ -403,3 +403,19 @@
 
 // !_______ события events
 
+const Emitter = require('events');
+
+const emitter = new Emitter();
+
+emitter.on('message', (data, second, third) => {
+  console.log('Вы прислалии сообщение ' + data);
+  console.log('Второй аргумент ' + second);
+})
+
+const MESSAGE = process.env.message || '';
+
+if (MESSAGE) {
+  emitter.emit('message', MESSAGE, 123)
+} else {
+  emitter.emit('message', 'Вы не указали сообщение')
+}
